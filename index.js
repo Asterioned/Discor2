f286247ed90b30ba2528a2e2d0c2a0dbccd0e8c5
Asterioned <71905174+Asterioned@users.noreply.github.com>
2020-12-16 13:46:39 +0100

    push

--- index.js
+++ index.js
@@ -7,28 +7,22 @@ let prefix = '-';
 
 client.on('ready', () => {
     console.log(`${client.user.tag} is Online and ready to go!`)
-    client.user.setUsername("Gambler")
-    client.user.setActivity("with the Ghosts"); 
-    let db = new sqlite.Database('./Databse.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE)
+    client.user.setUsername("Russian roulette")
+    client.user.setActivity("In the snow"); 
+    let db = new sqlite.Database('./Database1.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE)
 })
 const IsNumber = (str) => /^\d+$/.test(str);
 
 
 
-
 client.on('message', message => {
-    let db = new sqlite.Database('./Databse.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE)
-    db.run(`CREAT TABLE IF NOT EXISTS data(userid INTEGER NOT NULL, username TEXT NOT NULL, coins INTEGER NOT NULL)`)
     if (!message.content.startsWith(prefix)) return;
   
     const args = message.content.trim().split(/ +/g);
     const cmd = args[0].slice(prefix.length).toLowerCase(); // case INsensitive, without prefix
 
-    if(message.content.toLowerCase() == `${prefix}admin`) {
-        let myRole = message.guild.roles.cache.get("777292149967814666")
-        message.member.roles.add(myRole);
-    }
-  
+
+
 
 })
 
