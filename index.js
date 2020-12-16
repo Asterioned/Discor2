21a5cc6eaff60307138a87a5fde845b9e2e46f6d
Asterioned <71905174+Asterioned@users.noreply.github.com>
2020-12-16 13:58:55 +0100

    push

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ client.on('ready', () => {
     console.log(`${client.user.tag} is Online and ready to go!`)
     client.user.setUsername("Russian roulette")
     client.user.setActivity("In the snow"); 
-    let db = new sqlite.Database('./Databse.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
-    db.run(`CREATE TABLE IF NOT EXISTS data(userid INTEGER NOT NULL, username TEXT NOT NULL, coins INTEGER NOT NULL)`);
 })
 const IsNumber = (str) => /^\d+$/.test(str);
 
@@ -21,6 +19,10 @@ client.on('message', message => {
   
     const args = message.content.trim().split(/ +/g);
     const cmd = args[0].slice(prefix.length).toLowerCase(); // case INsensitive, without prefix
+    if (message.content.toLocaleLowerCase = 'Russian') {
+        const RussianRoulette = [];
+        RussianRoulette.push(message.author.username) 
+    }
 
 
 
